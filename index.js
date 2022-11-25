3d6e9a2edc57091815d4ee6d5616c502460a80d2
Liam Brooke <24947193+ItsLimaBean@users.noreply.github.com>
2022-11-25 14:32:29 -0800

    Done round then floor/ciel...

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
                 const now = new Date().getTime();
                 const diffMs = end - now;
 
-                const hoursMS = Math.round(diffMs / (60 * 60 * 1000));
+                const hoursMS = diffMs / (60 * 60 * 1000);
                 let hours = hoursMS < 0 ? Math.ciel(hoursMS) : Math.floor(hoursMS);
                 const minutes = Math.round(diffMs / (60 * 1000)) % 60;
 
